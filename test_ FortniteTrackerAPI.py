d446e0ceb3055d3be4638015a410489fa74464f4
sinaja <sina.jazayeri@schneider-electric.com>
2018-06-29 00:37:52 +0100

    removed depenency on INPUT class and added first test for SetUrl

--- test_ FortniteTrackerAPI.py
+++ test_ FortniteTrackerAPI.py
@@ -1,18 +1,7 @@
 import unittest
 from FortniteTrackerAPIClass import FortniteTrackerAPI
-from FortniteTrackerAPIClass import Input
 
-class testInput(Input):
-    testString = '' 
-    def __init__(self, testInput) :
-        self.testString = testInput
-
-    def GetInput(self, a):
-        return self.testString
-        
-myTestFortniteTrackerAPI = FortniteTrackerAPI()
+myTestFortniteTrackerAPI = FortniteTrackerAPI('xbl','AarashJ')
 class Test_FortniteTrackerAPI(unittest.TestCase) :
     def test_SetUrl(self):
-        myTestPlatform = testInput('xbl')
-        myTestUser = testInput('AarashJ')
-        self.assertEqual(myTestFortniteTrackerAPI.SetUrl(myTestPlatform, myTestUser), 'https://api.fortnitetracker.com/v1/profile/xbl/AarashJ')
+        self.assertEqual(myTestFortniteTrackerAPI.SetUrl(), 'https://api.fortnitetracker.com/v1/profile/xbl/AarashJ')
