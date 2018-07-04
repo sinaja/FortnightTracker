243c9c1e10932f4c0fa14c32c5f1aa63ed49433f
Aarash <aarash1.jazayeri@gmail.com>
2018-07-04 22:35:14 +0100

    Added test for GetUsername

--- tests/test_FortniteTrackerAPI.py
+++ tests/test_FortniteTrackerAPI.py
@@ -4,5 +4,8 @@ from source.FortniteTrackerAPIClass import FortniteTrackerAPI
 
 myTestFortniteTrackerAPI = source.FortniteTrackerAPIClass.FortniteTrackerAPI('xbl','AarashJ')
 class Test_FortniteTrackerAPI(unittest.TestCase) :
-    def test_SetUrl(self):
+    def test_SetUrl(self) :
         self.assertEqual(myTestFortniteTrackerAPI.SetUrl(), 'https://api.fortnitetracker.com/v1/profile/xbl/AarashJ')
+        
+    def test_GetUsername(self) :
+        self.assertEqual(myTestFortniteTrackerAPI.GetUsername('./tests/TestStatistics.txt'), 'AarashJ')
